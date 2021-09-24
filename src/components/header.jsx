7b7bbd46363d60715033b3d636aa6d06e4d45e14
Rafa <alu0101121901@ull.edu.es>
2021-09-24 14:22:05 +0100

    Header added

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+import '../styles/header.css'
+
+const Header = () => {
+
+
+  return (
+    <div class="wrapper">
+      <div class="typing-demo">
+        Este es mi poemario.
+      </div>
+    </div>
+  );
+}
+
+export default Header;
